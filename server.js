86aa0949691df2becc750e8bea076a90c8d6f0b5
garfield-mylee <garfield.mylee@gmail.com>
2024-09-18 06:27:59 +1200

    add cors

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
+
+
+app.use(cors());
 
 // Middleware
 app.use(bodyParser.json());
